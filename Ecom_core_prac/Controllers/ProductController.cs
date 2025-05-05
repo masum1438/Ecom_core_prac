911dc72241dfe60c4f3ec397d785da1933e364be
masum1438 <masumahmed1438@gmail.com>
2025-05-05 10:25:50 +0600

    with  card view and details.

---
+++ Ecom_core_prac/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecom_core_prac.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult SingleProduct()
+        {
+            //@Context.Request.Query["id"].ToString()
+            int id = Convert.ToInt32(HttpContext.Request.Query["id"].ToString());
+            return View();
+        }
+        public IActionResult CartDetails()
+        {
+            return View();
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
